935203ce9ace24a0a6c0811929c5082e9532a299
ARDScott <ardscott@hotmail.com>
2018-04-26 21:14:52 -0400

    Made Level 1 a whole lot prettier

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 20.0f;
+    [SerializeField] float rcsThrust = 250.0f;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -15,14 +18,31 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK"); // TODO: Remove
+                break;
+            case "Fuel":
+                print("FUEL"); // TODO: Remove
+                break;
+            default:
+                print("DEAD"); // TODO: Remove
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -32,13 +52,22 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.Pause();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // Take manual control of the rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate( Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // Resume physics control of the rotation
     }
 }
